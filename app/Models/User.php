9aeedcb210ef27be23e057aea81c9421ea7b83a0
Shohrux <shohruxnizomov170@gmail.com>
2025-11-15 16:53:50 +0500

    Maxfiy maydonlar (hidden) va casts yangilandi

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,18 +41,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_secret',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'skills' => 'array',
+        'two_factor_enabled' => 'boolean',
+        'deleted_at' => 'datetime',
+    ];
 }
