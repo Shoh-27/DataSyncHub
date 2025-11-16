c0e52c8c29bf528fe0c40444cc954546ffc9427d
Shohrux <shohruxnizomov170@gmail.com>
2025-11-16 10:52:32 +0500

    fillable , hidden  va casts maydonlari toldirildi

--- app/Models/EmailVerificationToken.php
+++ app/Models/EmailVerificationToken.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class EmailVerificationToken extends Model
 {
-    //
+    use HasFactory;
+
+    protected $connection = 'mysql';
+
+    protected $fillable = [
+        'user_id',
+        'token',
+        'expires_at',
+    ];
+
+    protected $casts = [
+        'expires_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
