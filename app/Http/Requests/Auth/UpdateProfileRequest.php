3f719144a35d06e602c139370d6a2183a54099ee
Shohrux <shohruxnizomov170@gmail.com>
2025-11-16 16:51:24 +0500

    updateprofile uchun FormRequest qoshildi

---
+++ app/Http/Requests/Auth/UpdateProfileRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateProfileRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+}
