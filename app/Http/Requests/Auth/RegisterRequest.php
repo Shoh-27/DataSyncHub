46c0944d73a38a5eaeafadc330dd46629074fa2d
Shohrux <shohruxnizomov170@gmail.com>
2025-11-16 11:01:06 +0500

    Register uchun FormRequest qoshildi

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
