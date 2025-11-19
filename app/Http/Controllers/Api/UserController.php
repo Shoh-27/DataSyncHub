e356a200c56ef39b0d9fd5387c164c86957be3a3
Shohrux <shohruxnizomov170@gmail.com>
2025-11-19 16:41:47 +0500

    feat(user): UserController karkasini qo‘shdim

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\UpdateProfileRequest;
+use App\Http\Resources\UserResource;
+use App\Services\UserService;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller
 {
-    //
+    public function __construct(
+        private UserService $userService
+    ) {}
+
+
 }
